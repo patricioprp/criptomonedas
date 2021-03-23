bbefa2c8de2621b6f3b5ff45f5a7d31f4137fe03
luca.tensolite <lucaf@tensolite.com>
2021-03-23 17:29:01 -0300

    se hace custom hooks

---
+++ src/hooks/useMoneda.js
@@ -0,0 +1,18 @@
+import React, {Fragment,useState} from 'react';
+
+const useMoneda = () => {
+
+    //state de nuestro custom hook
+    const [state, actualziarState] = useState('');
+   const Seleccionar = () =>(
+    <Fragment>
+        <label>Moneda</label>
+        <select>
+            <option value="AR">Peso Argentino</option>
+        </select>
+    </Fragment>
+   );
+   //Retornar state, interfaz y fn que modifica el state
+   return [state,Seleccionar,actualziarState]
+}
+export default useMoneda;
